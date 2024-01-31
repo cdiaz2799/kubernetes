367a357f19fee5b57d4a56825159ff02d055d6a5
Christian Diaz <christian@cdiaz.cloud>
2024-01-31 05:41:48 +0000

    Deploy/actual

--- cloudflared.tf
+++ cloudflared.tf
@@ -1,3 +1,19 @@
+resource "cloudflare_tunnel_config" "tunnel" {
+  account_id = data.onepassword_item.cloudflare.username
+  tunnel_id  = module.cloudflared_tunnel.cloudflared_tunnel_id
+  config {
+    ingress_rule {
+      hostname = cloudflare_record.actual.name
+      service  = "${kubernetes_service.actual.metadata[0].name}:${kubernetes_service.actual.spec[0].port[0].port}"
+    }
+
+    ingress_rule {
+      service = "http_status:404"
+    }
+  }
+
+}
+
 module "cloudflared_tunnel" {
   source = "./modules/cloudflared"
 
