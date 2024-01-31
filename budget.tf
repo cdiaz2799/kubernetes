4e3c569b0cabc377625f375d7b422f461758004d
Christian Diaz <christian@cdiaz.cloud>
2024-01-30 22:15:11 -0700

    Add Actual Budget

---
+++ budget.tf
@@ -0,0 +1,79 @@
+resource "kubernetes_deployment" "actual" {
+  metadata {
+    name      = "actual"
+    namespace = kubernetes_namespace.actual.metadata[0].name
+    labels = {
+      "app" = "actual"
+    }
+  }
+  spec {
+    replicas = 1
+    selector {
+      match_labels = {
+        "app" = "actual"
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          "app" = "actual"
+        }
+      }
+      spec {
+        container {
+          name  = "actual-budget"
+          image = "docker.io/actualbudget/actual-server:latest-alpine"
+
+          port {
+            container_port = 5006
+          }
+
+          resources {
+            requests = {
+              "memory" = "256Mi"
+            }
+            limits = {
+              "memory" = "512Mi"
+            }
+          }
+
+          volume_mount {
+            name       = "actual-data"
+            mount_path = "/data"
+          }
+        }
+        volume {
+          name = "actual-data"
+          persistent_volume_claim {
+            claim_name = kubernetes_persistent_volume_claim.actual.metadata[0].name
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_persistent_volume_claim" "actual" {
+  metadata {
+    name      = "budget-data"
+    namespace = kubernetes_namespace.actual.metadata[0].name
+    labels = {
+      "app" = "actual"
+    }
+  }
+  spec {
+    storage_class_name = kubernetes_storage_class.nfs.metadata[0].name
+    access_modes       = ["ReadWriteOnce"]
+    resources {
+      requests = {
+        storage = "1Gi"
+      }
+    }
+  }
+}
+
+resource "kubernetes_namespace" "actual" {
+  metadata {
+    name = "budget"
+  }
+}
